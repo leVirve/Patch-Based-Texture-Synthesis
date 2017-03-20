09810f7f86d4d7ba2511edfa578cf14507db071c
Hung Jin Lin <gae.m.project@gmail.com>
2017-03-20 23:14:45 +0800

    (fix) indexing error in texure patches - find better parameter for the implementation change

--- PatchBasedTextureTransfer.py
+++ PatchBasedTextureTransfer.py
@@ -11,7 +11,7 @@ texure = cv2.imread('textures/rice.png' if len(sys.argv) <= 2 else sys.argv[1])
 source = cv2.imread('src.jpg' if len(sys.argv) <= 2 else sys.argv[2])
 output = np.zeros(source.shape, np.uint8)
 patch_sz = 10
-init_threshold_const = 1
+init_threshold_const = 3
 
 
 def print_progress(done_patches, total_patches, threshold=0):
@@ -21,12 +21,10 @@ def print_progress(done_patches, total_patches, threshold=0):
 
 
 def extract_patches(img, patch_sz, overlap=None):
-    # img = img.astype(np.int32)
     h, w, _ = img.shape
     overlap = overlap or patch_sz
     rh, rw = (h // overlap) * overlap, (w // overlap) * overlap
     patches = [
-        # img[y:y + patch_sz, x:x + patch_sz]
         (y, x)
         for y in range(0, h - patch_sz, overlap)
         for x in range(0, w - patch_sz, overlap)
@@ -38,8 +36,10 @@ def extract_patches(img, patch_sz, overlap=None):
 def extract_texture_patches(tex, patch_sz):
     h, w, _ = tex.shape
     tex = tex.astype(np.int32)
+    h, w = (h // patch_sz) * patch_sz, (w // patch_sz) * patch_sz
+    tex = tex[:h, :w]
     patches = image.extract_patches_2d(tex, (patch_sz, patch_sz))
-    return patches[:h - patch_sz, :w - patch_sz]
+    return patches, h, w
 
 
 def get_best_texture_patch(src_patch, tex_patches, threshold):
@@ -60,10 +60,7 @@ def fill_output(img_px, output, tex_px, texure):
     x, y = img_px
     tx, ty = tex_px
     tex = texure[tx:tx + patch_sz, ty:ty + patch_sz]
-    try:
-        output[x:x + patch_sz, y:y + patch_sz] = tex
-    except:
-        pass
+    output[x:x + patch_sz, y:y + patch_sz] = tex
 
 
 def main():
@@ -71,7 +68,8 @@ def main():
     source = source.astype(np.int32)
 
     patches = extract_patches(source, patch_sz, 0)
-    tex_patches = extract_texture_patches(texure, patch_sz)
+    tex_patches, tex_h, tex_w = extract_texture_patches(texure, patch_sz)
+
     for num, patch in enumerate(patches):
         b, a = patch
         patch = source[b:b + patch_sz, a:a + patch_sz]
@@ -81,13 +79,14 @@ def main():
         p_ids = np.array([])
         while not p_ids.size:
             p_ids = get_best_texture_patch(patch, tex_patches, threshold)
-            threshold *= 1.05
+            threshold *= 1.1
 
         p_id = p_ids[randint(0, len(p_ids) - 1)]
-        j, i = ind2sub(p_id, texure.shape[1] - patch_sz)
+        j, i = ind2sub(p_id, tex_w - patch_sz + 1)
         y, x = ind2sub(num, source.shape[1] // patch_sz)
-        fill_output((y * patch_sz, x * patch_sz), output, (j, i), texure)
         print_progress(num + 1, len(patches), threshold)
+
+        fill_output((y * patch_sz, x * patch_sz), output, (j, i), texure)
 
     cv2.imwrite('output.png', output)
     cv2.imshow('output', output)
