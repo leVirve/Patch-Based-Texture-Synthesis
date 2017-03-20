4cf6c971e40814a9e0ec99094af378a0028f5cbb
Hung Jin Lin <gae.m.project@gmail.com>
2017-03-20 21:52:06 +0800

    minor changes on function interface

--- PatchBasedTextureTransfer.py
+++ PatchBasedTextureTransfer.py
@@ -7,10 +7,9 @@ import numpy as np
 from sklearn.feature_extraction import image
 
 
-# texure = cv2.imread('tex.jpg')
-# out = img = cv2.imread('meego.jpg')
-texure = cv2.imread('textures/rice.png')
-out = img = cv2.imread('src.jpg')
+texure = cv2.imread('textures/rice.png' if len(sys.argv) <= 2 else sys.argv[1])
+source = cv2.imread('src.jpg' if len(sys.argv) <= 2 else sys.argv[2])
+output = np.zeros(source.shape, np.uint8)
 patch_sz = 10
 init_threshold_const = 1
 
@@ -21,14 +20,16 @@ def print_progress(done_patches, total_patches, threshold=0):
     sys.stdout.flush()
 
 
-def extract_patches(img, patch_sz):
-    img = img.astype(np.int32)
+def extract_patches(img, patch_sz, overlap=None):
+    # img = img.astype(np.int32)
     h, w, _ = img.shape
-    rh, rw = (h // patch_sz) * patch_sz, (w // patch_sz) * patch_sz
+    overlap = overlap or patch_sz
+    rh, rw = (h // overlap) * overlap, (w // overlap) * overlap
     patches = [
-        img[y:y + patch_sz, x:x + patch_sz]
-        for y in range(0, h, patch_sz)
-        for x in range(0, w, patch_sz)
+        # img[y:y + patch_sz, x:x + patch_sz]
+        (y, x)
+        for y in range(0, h - patch_sz, overlap)
+        for x in range(0, w - patch_sz, overlap)
         if x < rw and y < rh
     ]
     return patches
@@ -36,7 +37,7 @@ def extract_patches(img, patch_sz):
 
 def extract_texture_patches(tex, patch_sz):
     h, w, _ = tex.shape
-    tex = texure.astype(np.int32)
+    tex = tex.astype(np.int32)
     patches = image.extract_patches_2d(tex, (patch_sz, patch_sz))
     return patches[:h - patch_sz, :w - patch_sz]
 
@@ -44,10 +45,11 @@ def extract_texture_patches(tex, patch_sz):
 def get_best_texture_patch(src_patch, tex_patches, threshold):
     diff = tex_patches - src_patch
     cost = np.sum(diff ** 2, axis=(1, 2, 3)) ** 0.5
-    hard_example = np.where(cost < threshold / 2)[0]
+    hard_example, = np.where(cost < threshold / 2)
     if hard_example.size:
         return hard_example
-    return np.where(cost < threshold)
+    soft_example, = np.where(cost < threshold)
+    return soft_example
 
 
 def ind2sub(n, width):
@@ -58,28 +60,38 @@ def fill_output(img_px, output, tex_px, texure):
     x, y = img_px
     tx, ty = tex_px
     tex = texure[tx:tx + patch_sz, ty:ty + patch_sz]
-    output[x:x + patch_sz, y:y + patch_sz] = tex
+    try:
+        output[x:x + patch_sz, y:y + patch_sz] = tex
+    except:
+        pass
 
 
 def main():
-    patches = extract_patches(img, patch_sz)
+    global source
+    source = source.astype(np.int32)
+
+    patches = extract_patches(source, patch_sz, 0)
     tex_patches = extract_texture_patches(texure, patch_sz)
     for num, patch in enumerate(patches):
+        b, a = patch
+        patch = source[b:b + patch_sz, a:a + patch_sz]
+
         threshold = init_threshold_const * (patch_sz ** 2)
 
         p_ids = np.array([])
         while not p_ids.size:
-            p_ids = get_best_texture_patch(patch, tex_patches, threshold)[0]
-            threshold *= 1.1
+            p_ids = get_best_texture_patch(patch, tex_patches, threshold)
+            threshold *= 1.05
 
         p_id = p_ids[randint(0, len(p_ids) - 1)]
         j, i = ind2sub(p_id, texure.shape[1] - patch_sz)
-        y, x = ind2sub(num, img.shape[1] // patch_sz)
-        fill_output((y * patch_sz, x * patch_sz), out, (j, i), texure)
-        # print(num, threshold, len(p_ids))
-        print_progress(num, len(patches), threshold)
+        y, x = ind2sub(num, source.shape[1] // patch_sz)
+        fill_output((y * patch_sz, x * patch_sz), output, (j, i), texure)
+        print_progress(num + 1, len(patches), threshold)
 
-    cv2.imwrite('output.png', out)
+    cv2.imwrite('output.png', output)
+    cv2.imshow('output', output)
+    cv2.waitKey()
 
 if __name__ == '__main__':
     main()
